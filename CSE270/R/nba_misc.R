577d60ec2d61882b1e05145b06951289ad781495
Habet <Habet@users.noreply.github.com>
2017-09-16 12:50:33 +0400

    Add files via upload

---
+++ CSE270/R/nba_misc.R
@@ -0,0 +1,20 @@
+#' Miscellaneous data on NBA team statistics, season 2016-2017
+#'
+#' @docType data
+#'
+#' @usage data(nba_misc)
+#'
+#' @format A data frame with 30 rows and 26 variables
+#'
+#' For variable definitions, look at the source
+#'
+#' variables with suffix _o, are showing offensive statistics
+#'
+#' variables with suffix _d, are showing defensive statistics
+#'
+#' https://www.basketball-reference.com/leagues/NBA_2017.html#misc_stats
+#'
+#'
+#' @keywords nba, skill_luck
+#'
+"nba_misc"
