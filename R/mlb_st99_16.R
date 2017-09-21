e47145129d0f6c547e1f0371abf53e0af805f67d
Habet <Habet@users.noreply.github.com>
2017-09-21 18:27:00 +0400

    added final standings for MLB

---
+++ R/mlb_st99_16.R
@@ -0,0 +1,24 @@
+#' MLB final standings, 1999-2017, Regular season
+#'
+#' @docType data
+#'
+#' @usage data(mlb_st99_16)
+#'
+#' @format A data frame with 540 rows and 6 variables
+#'
+#' Team: team name
+#'
+#' Lig: The conference/League - AL: American League, NL: National League
+#'
+#' Div: Division: ED-Eastern Division, WD: Western Division, CD: Central Division
+#'
+#' W: Number of wins
+#'
+#' L: number of lost games
+#'
+#' season: The season
+#'
+#' @keywords mlb, competitive balance, skill and luck
+#'
+#' @source \href{https://www.baseball-reference.com}{www.baseball-reference.com}
+"mlb_st99_16"
