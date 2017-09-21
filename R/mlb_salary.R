a42d629f7374abf55b338b9f1b9a7da9ea6ab659
Habet <Habet@users.noreply.github.com>
2017-09-21 20:52:41 +0400

    added file with salary information from MLB

---
+++ R/mlb_salary.R
@@ -0,0 +1,24 @@
+#' MLB salaries, 1999-2016
+#'
+#' @docType data
+#'
+#' @usage data(mlb_salary)
+#'
+#' @format A data frame with 540 rows and 6 variables
+#'
+#' Team: team name
+#'
+#' Opening.day: Opening day salary
+#'
+#' Current: Current Season Salary
+#'
+#' Avg: Average salary
+#'
+#' Median: Median salary
+#'
+#' season: The season
+#'
+#' @keywords mlb, competitive balance
+#'
+#' @source \href{https://www.usatoday.com/sports/mlb/salaries/2016/team/all/}{USATODAY}
+"mlb_salary"
