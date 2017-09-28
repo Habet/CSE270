a8f6cfb554b4eb20fe4889d2e7dff33c4b5a2c61
Habet <Habet@users.noreply.github.com>
2017-09-28 18:06:58 +0400

    added nba12 and nba12p for Adjusted Plus/Minus

---
+++ R/nba12p.R
@@ -0,0 +1,13 @@
+#' NBA player statistics for 2011-2012 from BasketValue
+#'
+#' @docType data
+#'
+#' @usage data(nba12p)
+#'
+#' @format A data frame with 479 rows and 39 variables
+#'
+#'
+#' @keywords NBA, Plus/Minus, Adjusted Plus/Minus
+#'
+#' @source BasketValue
+"nba12p"
