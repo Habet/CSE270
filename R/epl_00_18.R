ea689b17f781e2804053df31913310c2ad6cdb65
Habet <Habet@users.noreply.github.com>
2018-07-20 21:52:43 +0400

    Add files via upload

---
+++ R/epl_00_18.R
@@ -0,0 +1,23 @@
+#' Data on English Premier League, 2000-2018:
+#' @docType data
+#'
+#' @usage data(epl_00_18)
+#'
+#' @format A data frame with 6840 rows and 6 variables
+#'
+#' Home: The name of the home team
+#'
+#' Away: The name of the away team
+#'
+#' HG: Full Time Home Team Goals
+#'
+#' AG: Full Time Away Team Goals
+#'
+#' Round: the number of the round (34 overall)
+#'
+#' The Season when the match was played (eg, season 2013/2014 is given as 2014)
+#'
+#' @keywords football, Elo ratings
+#'
+#' @source \href{http://www.worldfootball.net}{WorldFootball.net}
+"epl_00_18"
