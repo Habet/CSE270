c2e1a66f551aa16b6768ec04dfa4faf88c5eb017
Habet <Habet@users.noreply.github.com>
2018-09-14 22:37:06 +0400

    Add files via upload

--- R/final_table.R
+++ R/final_table.R
@@ -3,18 +3,18 @@
 #' @aliases final_table
 #' @description Creates final league table
 #' @param data dataframe with the same structure as f_data_sm
-#' @param league The league for which the table is created
+#' @param country The country for which the table is created
 #' @param season the season for which the table is returned, season 2017/2018 will be given as 2018
 #' @import dplyr
 #' @return dataframe with the final league standing
 #' @export
 
-final_table <-  function (data, league, season) {
-  sa <- data %>% filter(SEASON == season, LEAGUE == league) %>%
+final_table <-  function (data, country, season) {
+  sa <- data %>% filter(SEASON == season, COUNTRY == country) %>%
     group_by(HOMETEAM) %>% summarise(W = sum(FTR == "H"),
                                      D = sum(FTR == "D"), L = sum(FTR == "A"), GF = sum(FTHG),
                                      GA = sum(FTAG))
-  sa1 <- data %>% filter(SEASON == season, LEAGUE == league) %>%
+  sa1 <- data %>% filter(SEASON == season, COUNTRY == country) %>%
     group_by(AWAYTEAM) %>% summarise(W = sum(FTR == "A"),
                                      D = sum(FTR == "D"), L = sum(FTR == "H"), GF = sum(FTAG),
                                      GA = sum(FTHG))
