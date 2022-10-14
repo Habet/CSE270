2ccf7d302f1f66893abf2522c703f30de570bdb1
Narek Sahakyan <nsahakyan>
2022-10-14 16:42:13 +0400

    fix rankings in nfl and mlb

---
+++ fix_data.R
@@ -0,0 +1,21 @@
+install.packages("dplyr")
+library(dplyr)
+
+load(file = "data/nfl_st.rda")
+
+nfl_st[nfl_st$T == -1,] <- 0
+
+save(nfl_st, file = "data/nfl_st.rda")
+
+
+load(file = "data/mlb_standings.rda") 
+
+mlb_standings <- mlb_standings %>%
+    group_by(Season) %>%
+    arrange(desc(W)) %>%
+    mutate(Rank = 1:n()) %>%
+    ungroup() %>%
+    arrange(Season)
+
+save(mlb_standings, file = "data/mlb_standings.rda")
+
