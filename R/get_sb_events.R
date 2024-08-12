5c3798094352ba528a3869192e4ec03012c4b74a
Narek Sahakyan <sahNarek>
2024-08-12 18:31:20 +0400

    wip: data collection, all set nfl odds missing

---
+++ R/get_sb_events.R
@@ -0,0 +1,13 @@
+library(StatsBombR)
+library(SBpitch)
+get_events <- function(MatchesDF) {
+  cl <- makeCluster(detectCores())
+  registerDoParallel(cl)
+  events.df <- foreach(i = 1:dim(MatchesDF)[1], .combine = bind_rows,
+      .multicombine = TRUE, .errorhandling = "remove",
+      .export = c("get.matchFree"), .packages = c("httr",
+        "jsonlite", "dplyr")) %dopar% {
+      get.matchFree(MatchesDF[i, ])
+  }
+  stopCluster(cl)
+  return (allclean(events.df))
